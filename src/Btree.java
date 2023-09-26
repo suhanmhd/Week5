d20d6ca844719934dd200677e793253c701b93f9
suhanmhd <suhanmuhammed4@gmail.com>
2023-09-26 11:03:52 +0530

    binary tree problems

--- src/Btree.java
+++ src/Btree.java
@@ -51,6 +51,24 @@ public  void inorderTraversal(Node root){
     inorderTraversal(root.right);
 }
 
+public void preorderTreversal(Node root){
+        if(root==null){
+           return;
+        }
+    System.out.println(root.data+" ");
+        preorderTreversal(root.left);
+        preorderTreversal(root.right);
+}
+
+    public void postorderTraversal(Node root) {
+        if (root == null) {
+            return;
+        }
+        postorderTraversal(root.left);
+        postorderTraversal(root.right);
+        System.out.println(root.data + " ");
+    }
+
 
 
 public  Node deleteTree(Node troot,int val){
@@ -88,6 +106,8 @@ public  Node deleteTree(Node troot,int val){
    return  troot;
 }
 
+
+
 public  Node inorderSuccessor(Node root){
         while(root.left!=null){
             root=root.left;
@@ -95,11 +115,28 @@ public  Node inorderSuccessor(Node root){
         return root;
 }
 
+
+
+
+
+
+    public int getLength(Node root) {
+        if (root == null) {
+            return 0;
+        }
+            int leftLength = getLength(root.left);
+            int rightLength = getLength(root.right);
+
+            // Add 1 to include the root node in the count
+            return 1+Math.max(leftLength , rightLength);
+
+    }
     public static void main(String[] args) {
         Btree btree = new Btree();
         btree.insert(10);
         btree.insert(12);
         btree.insert(7);
+
         btree.insert(3);
         btree.insert(23);
         btree.insert(4);
@@ -107,8 +144,14 @@ public  Node inorderSuccessor(Node root){
 //        btree.deleteTree(btree.root, 4);
 //        System.out.println();
 //        btree.inorderTraversal(btree.root);
-        btree.deleteTree(btree.root, 10);
+//        btree.deleteTree(btree.root, 10);
+//        System.out.println();
+//        btree.inorderTraversal(btree.root);
+//        System.out.println();
+//        btree.preorderTreversal(btree.root);
+//        System.out.println();
+//        btree.postorderTraversal(btree.root);
         System.out.println();
-        btree.inorderTraversal(btree.root);
+        System.out.println(btree.getLength(btree.root));
     }
 }
--- src/graphs/Graph.java
+++ src/graphs/Graph.java
@@ -2,6 +2,7 @@ package graphs;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class Graph {
@@ -57,7 +58,43 @@ public class Graph {
           }
       }
   }
-  public  void DFS(){
+
+
+
+    public List<Integer> findSinglePath(int start, int end) {
+        List<Integer> path = new ArrayList<>();
+        boolean[] visited = new boolean[v];
+        boolean foundPath = false;
+        findSinglePathHelper(start, end, path, visited, foundPath);
+        return path;
+    }
+
+    private boolean findSinglePathHelper(int curr, int end, List<Integer> path, boolean[] visited, boolean foundPath) {
+
+        if(curr == end){
+            path.add(curr);
+            foundPath = true;
+            return true;
+        }
+
+        visited[curr] = true;
+        path.add(curr);
+
+        for(int i = 0; i < graph[curr].size(); i++){
+            Edge e = graph[curr].get(i);
+            if(!visited[e.dest] && !foundPath){
+                if(findSinglePathHelper(e.dest, end, path, visited, foundPath)){
+                    return true;
+                }
+            }
+        }
+
+        visited[curr] = false;
+        path.remove(path.size()-1);
+        return false;
+    }
+
+    public  void DFS(){
        boolean[] vis = new boolean[v];
        for(int i=0;i<v;i++){
            if(!vis[i]){
@@ -80,115 +117,82 @@ public class Graph {
       }
   }
 
-    public void findallpath(int start, int end) {
-        ArrayList<Integer> path = new ArrayList<>();
-//        String path="";
-        boolean[] visited = new boolean[v];
-        findallpathHelper(start, end, path, visited);
-    }
 
-    private void findallpathHelper(int curr, int end, ArrayList<Integer> path, boolean[] visited) {
+//    public void findallpath(int start, int end) {
+//        ArrayList<Integer> path = new ArrayList<>();
+////        String path="";
+//        boolean[] visited = new boolean[v];
+//        findallpathHelper(start, end, path, visited);
+//    }
+//
+//    private void findallpathHelper(int curr, int end, ArrayList<Integer> path, boolean[] visited) {
+//
+//        if (curr == end) {
+//            System.out.println(path);
+//            return;
+//        }
+//        for (int i = 0; i < graph[curr].size(); i++) {
+//            Edge e = graph[curr].get(i);
+//            if (!visited[e.dest]) {
+//                visited[curr] = true;
+//                path.add(e.dest);
+//                findallpathHelper(e.dest, end, path, visited);
+//                visited[curr] = false;
+//            }
+//        }
+//    }
 
-       if(curr==end){
-           System.out.println(path);
-           return;
-       }
-        for(int i=0;i<graph[curr].size();i++){
-            Edge e = graph[curr].get(i);
-            if(!visited[e.dest]){
-                visited[curr]=true;
-                 path.add(e.dest);
-                findallpathHelper( e.dest, end,  path, visited);
-                visited[curr]=false;
-            }
-        }
-
-
-
-    }
-
-
-
-
-    public static void main(String[] args) {
-        Graph g = new Graph(7);
-        g.addedges(0,1);
-        g.addedges(0,2);
-        g.addedges(1,0);
-        g.addedges(1,3);
-        g.addedges(2,0);
-        g.addedges(2,4);
+        public static void main (String[]args){
+            Graph g = new Graph(7);
+            g.addedges(0, 1);
+            g.addedges(0, 2);
+            g.addedges(1, 0);
+            g.addedges(1, 3);
+            g.addedges(2, 0);
+            g.addedges(2, 4);
 //        g.addedges(2,3);
 //        g.addedges(2,1);
-        g.addedges(3,1);
-        g.addedges(3,4);
-        g.addedges(3,5);
-        g.addedges(4,2);
-        g.addedges(4,3);
-        g.addedges(4,5);
-        g.addedges(5,3);
-        g.addedges(5,4);
-        g.addedges(5,6);
+            g.addedges(3, 1);
+            g.addedges(3, 4);
+            g.addedges(3, 5);
+            g.addedges(4, 2);
+            g.addedges(4, 3);
+            g.addedges(4, 5);
+            g.addedges(5, 3);
+            g.addedges(5, 4);
+            g.addedges(5, 6);
 //        g.addedges(2,1);
 
-        g.printGraph();
+//        g.printGraph();
 //        g.removeEdge(2,3);
 //        g.removeEdge(2,4);
 //        System.out.println();
 //        g.printGraph();
-        g.BFS();
-        System.out.println();
-        g.DFS();
-        g.findallpath(0,5);
+            g.BFS();
+            System.out.println();
+            g.DFS();
+            System.out.println(g.findSinglePath(3,5));
+
+        }
+
+        public void printGraph () {
+            for (int i = 0; i < v; i++) {
+                System.out.print(" vertex " + i + " :");
+                for (Edge a : graph[i]) {
+                    System.out.print("(" + a.src + "," + a.dest + ")");
+                }
+                System.out.println();
+            }
+        }
+
+        public void removeEdge ( int src, int dest){
+            for (int i = 0; i < graph[src].size(); i++) {
+                if (graph[src].get(i).dest == dest) {
+                    graph[src].remove(i);
+                    break;
+                }
+            }
+        }
 
     }
 
-    public  void printGraph(){
-       for(int i=0;i<v;i++){
-           System.out.print(" vertex "+ i +" :");
-           for (Edge a :graph[i]){
-               System.out.print("("+a.src+ ","+a.dest+")");
-           }
-           System.out.println();
-       }
-    }
-
-    public  void removeEdge(int src,int dest){
-       for(int i=0;i<graph[src].size();i++){
-          if(graph[src].get(i).dest==dest){
-              graph[src].remove(i);
-              break;
-          }
-       }
-    }
-
-//  public void BFS() {
-//       boolean[] visited = new boolean[v];
-//       Queue<Integer>q = new LinkedList<>();
-//
-//       for (int i=0;i<v;i++){
-//           if(!visited[i]){
-//               q.add(i);
-//               visited[i]=true;
-//
-//               while (!q.isEmpty()){
-//                   int curr = q.remove();
-//                   System.out.print(curr+"  ");
-//
-//                   for(int j=0;j<graph[curr].size();j++){
-//                       Edge e = graph[curr].get(j);
-//                       if(!visited[e.dest]){
-//                           q.add(e.dest);
-//                           visited[e.dest]=true;
-//                       }
-//                   }
-//               }
-//
-//
-//
-//           }
-//       }
-//
-//
-//}
-}
